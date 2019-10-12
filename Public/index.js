61008e503b052c59ac6f458061a9ca82c1b2da9a
Uchey <ucheuzochukwumark@gmail.com>
2019-10-12 23:42:10 +0100

    populate all patients from db to page

--- Public/index.js
+++ Public/index.js
@@ -20,8 +20,39 @@ $(document).ready(() => {
             })
         }
         return false;
-    })
+    });
 });
+
+ //get items from database
+$(document).ready(() => {
+    var id = sessionStorage.getItem("user");
+   
+    $.ajax({
+      method: 'GET',
+      url: "http://localhost:3000/patients",
+      dataType: 'json'
+    }).done(data => {
+      $.map(data, patient => {
+        let add = '<tr>';
+        add += '<th scope="row">' + parseInt(data.indexOf(patient) + 1) + '</th>';
+        add += '<td><a class="clientName" data-toggle="modal" data-target="#dataModal" onclick="displayPatient(${patient.id})">' + patient.name + '</a></td>';
+        add += '<td>' + patient.addresss + '</td>'
+        add += '<td>' + patient.age + '</td>'
+        add += '<td>' + patient.PhoneNumber + '</td>'
+        add += '<td>' + patient.diagnosisDescription + '</td>'
+        add += '<td class="text-center">' + `<button id="" class="d-none d-sm-inline btn btn-sm btn-warning shadow-sm update main-color-bg" data-toggle="modal" data-target="#updatModal"> Update</button>` + '</td>'
+        add += '<td class="text-center">' + `<button  class="d-none d-sm-inline btn btn-sm btn-danger shadow-sm remove main-color-bg tableWist type="button" data-target="#deleteModal" data-toggle="modal">Delete</button>` + '</td>'
+        add += '</tr>';
+  
+        $('table tbody').append(add);
+      });
+      if (data.length < 1) {
+        $("p").append("You don't have any leave record")
+      }
+
+    })
+})
+
   
     
   
