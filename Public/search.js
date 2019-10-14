50f229bb7ff99189e22004462863300d80988f02
Uchey <ucheuzochukwumark@gmail.com>
2019-10-14 01:34:36 +0100

    modify search page to sort with phone number

--- Public/search.js
+++ Public/search.js
@@ -4,5 +4,25 @@ $(document).ready(() => {
     $("form").submit((event) => {
         event.preventDefault();
         let phoneNumber = $("#phoneNumber").val();
+       
+        $.ajax({
+            type: "GET",
+            url: `http://localhost:3000/patients?phoneNumber=${phoneNumber}`,
+            success: function (data, patient) {
+                console.log(data)
+                $("table").show();
+                let add = '<tr>';
+                add += '<th scope="row">' + data[0].id + '</th>';
+                add += '<td>' +  data[0].name + '</td>';
+                add += '<td>' +  data[0].age + '</td>'
+                add += '<td>' +  data[0].address + '</td>'
+                add += '<td>' +  data[0].PhoneNumber + '</td>'
+                add += '<td>' +  data[0].bloodGroup + '</td>'
+                add += '<td>' +  data[0].genotype + '</td>'
+                add += '</tr>'; 
+ 
+                $('table tbody').append(add);
+            }
+        });
     });
 }); 
